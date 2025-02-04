79c90bd580911a189353cefa94dcde33e0ec2ed0
seongho <412jsh@naver.com>
2025-02-04 17:47:41 +0900

    조금 수정

--- src/containers/review/SearchCon.js
+++ src/containers/review/SearchCon.js
@@ -4,22 +4,29 @@ import { useEffect, useState } from "react"
 import { getSearchList } from "../../service/review"
 
 const SearchCon = () => {
-    const [params] = useSearchParams()
+    const [params] = useSearchParams() // URL 쿼리 파라미터 가져오기
     const [list, setList] = useState([])
-    useEffect(()=>{
+
+    useEffect(() => {
+        const id = params.get("id") // URL에서 id 값 가져오기
+        if (!id) {
+            console.error("유효한 ID가 없습니다.")
+            return
+        }
+
         const getData = async () => {
-            try{
-                const data= await getSearchList(params.get("id"))
+            try {
+                const data = await getSearchList(id) // id를 전달하여 데이터 가져오기
                 setList(data)
-            } catch (error){
+            } catch (error) {
                 console.error("데이터 가져오기 오류:", error)
             }
         }
         getData()
-    },[params.get("id")])
+    }, [params]) // params가 변경될 때마다 다시 실행
 
-    return<>
-       <SearchCom list={list} id={params.get("id")}/>
+    return <>
+        <SearchCom list={list} id={params.get("id")} />
     </>
 }
 
--- src/service/review.js
+++ src/service/review.js
@@ -1,10 +1,20 @@
 const getSearchList = async (id) => {
-    const res = await fetch("/project/review/search/"+id)
-    return await res.json()
+    if (id === null || id === undefined) {
+        throw new Error("Invalid ID");
+    }
+    console.log("getSearchList => ",id)
+    const res = await fetch("http://localhost:8080/root/review/search/" + id,{ 
+        method: 'GET', // GET 메서드 사용
+        mode: 'cors'  // CORS 모드 설정
+        });
+    return await res.json();
 }
 
 const getReviewList = async (id,start) => {
-    const res = await fetch("/project/review/info?id="+id+"&start="+start)
+    const res = await fetch("http://localhost:8080/root/review/info?id="+id+"&start="+start,{ 
+        method: 'GET', // GET 메서드 사용
+        mode: 'cors'  // CORS 모드 설정
+        });
     return await res.json()
 }
 
