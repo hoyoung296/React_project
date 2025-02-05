cc419b7d4cb33220ea07fcd43edcd678b8be3932
sodam <wdam0910@naver.com>
2025-02-05 17:06:06 +0900

    메인페이지 로고 검색창 수정

--- src/components/common/HeaderCom.js
+++ src/components/common/HeaderCom.js
@@ -1,13 +1,23 @@
 import '../../css/main.css';
+import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
 
 function HeaderCom({ onChange, mySubmit, input}) {
+    const location = useLocation();
+    const isHomePage = location.pathname === '/';
+
     return <>
-        <header className='header_body'>
-            <div className='logo'>MOVIE</div>
-            <form onSubmit={mySubmit}>
+        <header className={`header_body ${isHomePage ? 'homepage_header' : ''}`}>
+            <Link to="/" className="logo">TheFillm</Link>
+            <form onSubmit={mySubmit} className='searchForm'>
                 <input type="text" className='search' value={input.search} name="search" onChange={onChange} />
+                <button className='search_btn'>
+                    <img className='search_btn' src='/img/search.png'/>
+                </button>
+                
             </form>
-            <div className='profile_img'>프사</div>
+            <div className='profile_img'></div>
         </header>
     </>
 };
