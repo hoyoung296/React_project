5e75838787fd4d3b7e76a9346be550ac1bf0b230
sodam <wdam0910@naver.com>
2025-02-13 12:42:49 +0900

    예매날짜선택 파일 생성

--- src/components/ticket/TicketDateCom.js
+++ src/components/ticket/TicketDateCom.js
@@ -1,17 +1,39 @@
-import '../../css/main.css';
+import { useState, useEffect } from "react";
+import "../../css/main.css";
 
 const TicketDateCom = () => {
-    return <div className='ticketCon'>
-        <div className='ticketDate'>
-            관람일자
+    const dummyShowtimes = [
+        "2025 - 02 - 13", "2025 - 02 - 14", "2025 - 02 - 15", "2025 - 02 - 16", "2025 - 02 - 17",
+        "2025 - 02 - 18", "2025 - 02 - 19", "2025 - 02 - 20", "2025 - 02 - 21", "2025 - 02 - 22"
+    ];
+
+    const [selectedDate, setSelectedDate] = useState("");
+
+    useEffect(() => {
+        setSelectedDate("");
+    }, []);
+
+    const handleSelect = (date) => {
+        setSelectedDate(prev => (prev === date ? "" : date));
+    };
+
+    return (
+        <div className="ticketCon">
+            <div className="ticketDate">
+                {dummyShowtimes.map((date, index) => (
+                    <button
+                        key={index}
+                        className={selectedDate === date ? "selected" : ""}
+                        onClick={() => handleSelect(date)}
+                    >
+                        {date}
+                    </button>
+                ))}
+            </div>
+            <div className="cinema">상영관</div>
+            <div className="buyTicket">선택내용</div>
         </div>
-        <div className='cinema'>
-            상영관
-        </div>
-        <div className='buyTicket'>
-            선택내용
-        </div>
-    </div>;
-}
+    );
+};
 
 export default TicketDateCom;
