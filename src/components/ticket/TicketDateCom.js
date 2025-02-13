731606683bdf99f839ff7333930cdf7992adfef4
sodam <wdam0910@naver.com>
2025-02-13 16:21:30 +0900

    예매페이지 연동 오류

--- src/components/ticket/TicketDateCom.js
+++ src/components/ticket/TicketDateCom.js
@@ -1,17 +1,25 @@
 import { useState, useEffect } from "react";
+import Axios from "axios";
 import "../../css/main.css";
 
 const TicketDateCom = () => {
-    const dummyShowtimes = [
-        "2025 - 02 - 13", "2025 - 02 - 14", "2025 - 02 - 15", "2025 - 02 - 16", "2025 - 02 - 17",
-        "2025 - 02 - 18", "2025 - 02 - 19", "2025 - 02 - 20", "2025 - 02 - 21", "2025 - 02 - 22"
-    ];
-
+    const [showtimes, setShowtimes] = useState([]); // 날짜 데이터 저장
     const [selectedDate, setSelectedDate] = useState("");
 
     useEffect(() => {
-        setSelectedDate("");
+        const fetchShowtimes = async (title) => {
+            try {
+                const response = await Axios.get("http://192.168.0.91:8080/root/member/schedule/title", {params : {title}});
+                setShowtimes(response.data.map(item => item.SRATDATE)); // 데이터를 상태에 저장
+                console.log("날짜 : ",showtimes);
+            } catch (error) {
+                console.error("날짜 데이터를 가져오는 데 실패했습니다.", error);
+            }
+        };
+    
+        fetchShowtimes(); // 함수 호출
     }, []);
+
 
     const handleSelect = (date) => {
         setSelectedDate(prev => (prev === date ? "" : date));
@@ -20,7 +28,7 @@ const TicketDateCom = () => {
     return (
         <div className="ticketCon">
             <div className="ticketDate">
-                {dummyShowtimes.map((date, index) => (
+                {showtimes.map((date, index) => (
                     <button
                         key={index}
                         className={selectedDate === date ? "selected" : ""}
