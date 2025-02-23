dc5d029318ad151803f0261f0f39e4125c83d6ae
sodam <wdam0910@naver.com>
2025-02-23 19:29:53 +0900

    로그인페이지 작업중

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,27 +1,12 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import '../css/login.css';
+import '../css/main.css';
+import LoginPageCom from '../components/login/LoginPageCom';
 
 function LoginPage() {
-    const navigate = useNavigate();
-
-    const handleLogin = () => {
-        //로그인 성공하면(아이디 패스워드 일치한지 확인)
-        navigate('/');                                                                                                                                                                                                      
-    };
     return (
-    <div className='login_body'>
-        <div className='login'>
-            <div className='title_movie'>MOVIE</div>
-            <div className='login_from'>
-                <input type="text" className='input_id' required placeholder=" ID"/><br/>
-                <input type="password" className='input_pwd' required placeholder=" PW"/>
-                <button className='login_btn' onClick={handleLogin}>Login</button>
-            </div>
-            
-        </div>
-    </div>
-)
+        <>
+            <LoginPageCom />
+        </>
+        );
 };
 
 export default LoginPage;
