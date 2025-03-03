c99e36f8d8856e1235578c55e6fb91c6a9638fe0
hoyoung <skghdud024@naver.com>
2025-03-03 10:14:44 +0900

    Feat : 관리자페이지영화관리수동추가완료

--- src/components/Admin/AdminMovieCom.js
+++ src/components/Admin/AdminMovieCom.js
@@ -1,12 +1,13 @@
 import AdminSidebar from "./AdminSidebar"
 import "../../css/admin/admin.css"
 
-const AdminMovieCom = ({ list, editMovie, InputChange, EditClick, Update, manualUpdate }) => {
+const AdminMovieCom = ({ list, editMovie, InputChange, EditClick, Update, manualUpdate, show, hide, handleChange, manualinsert, newMovie }) => {
     return (
         <div className="admindiv">
             <AdminSidebar activeLink="영화관리" />
             <div className="admindiv-1">
-                <h1>영화관리</h1><br />
+                <h1>영화관리</h1>
+                <button onClick={show}>추가</button>
                 <div className="table-wrapper">
                     <table align="center">
                         <thead>
@@ -27,7 +28,7 @@ const AdminMovieCom = ({ list, editMovie, InputChange, EditClick, Update, manual
                             {list && list.length > 0 ? (
                                 list.map((data) => (
                                     <tr key={data.movieId}>
-                                         <td>
+                                        <td>
                                             {editMovie?.movieId === data.movieId ? (
                                                 <input type="text" name="movieId" value={data.movieId} onChange={(e) => InputChange(e, data.movieId)} />
                                             ) : (
@@ -115,6 +116,26 @@ const AdminMovieCom = ({ list, editMovie, InputChange, EditClick, Update, manual
                             )}
                         </tbody>
                     </table>
+                </div>
+            </div>
+            <div className="modal">
+                <div style={{ height: "45%", bottom: "30%" }}>
+                    <span onClick={hide}>X</span><br />
+                    <h1>영화 추가</h1>
+                    <form onSubmit={manualinsert}>
+                        <input type="text" name="movieId" placeholder="영화 ID" value={newMovie.movieId} onChange={handleChange} required />
+                        <input type="text" name="title" placeholder="영화 제목" value={newMovie.title} onChange={handleChange} required />
+                        <input type="text" name="entitle" placeholder="영어 제목" value={newMovie.entitle} onChange={handleChange} required />
+                        <input type="text" name="posterUrl" placeholder="포스터 URL" value={newMovie.posterUrl} onChange={handleChange} required />
+                        <input type="text" name="stillUrl" placeholder="스틸컷 URL" value={newMovie.stillUrl} onChange={handleChange} required />
+                        <textarea name="movieSynopsis" placeholder="시놉시스" value={newMovie.movieSynopsis} onChange={handleChange} required />
+                        <input type="text" name="directorName" placeholder="감독" value={newMovie.directorName} onChange={handleChange} required />
+                        <input type="text" name="actors" placeholder="배우" value={newMovie.actors} onChange={handleChange} required />
+                        <input type="text" name="movieRank" placeholder="순위 (YYYYMMDD-R)" value={newMovie.movieRank} onChange={handleChange} required />
+                        <input type="text" name="openDt" placeholder="개봉일 (YYYY-MM-DD)" value={newMovie.openDt} onChange={handleChange} required />
+                        <input type="text" name="runtime" placeholder="러닝타임" value={newMovie.runtime} onChange={handleChange} required /><br /><br />
+                        <button>등록</button>
+                    </form>
                 </div>
             </div>
         </div>
