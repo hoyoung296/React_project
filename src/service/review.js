dc7786261c1ddbee77d2fb0bf68f1a128b67f96c
ymjeon1006 <ymjeon1006@naver.com>
2025-03-04 15:00:30 +0900

    pull

--- src/service/review.js
+++ src/service/review.js
@@ -2,9 +2,6 @@ import axios from "axios"
 
 const BASE_URL = "http://localhost:8080/root/review"
 
-// 학원에서만 다른사람 노트북 아이피로 연결 (박소담 전용) 
-// const BASE_URL = "http://192.168.0.91:8080/root/review"
-
 // 검색 결과 가져오기
 const getSearchList = async (id) => {
     try {
