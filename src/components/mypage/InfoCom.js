68850ef763e568a2629fd1a2fb7e5a8c197e6dba
sodam <wdam0910@naver.com>
2025-03-07 17:45:34 +0900

    마이페이지 회원정보수정 css작업중

--- src/components/mypage/InfoCom.js
+++ src/components/mypage/InfoCom.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import '../../css/login.css';
+import MypageSidebar from "../common/MypageSidebar"
+import '../../css/mypage.css';
 
 function InfoCom() {
     return (
-    <div>
-        마이페이지-회원정보 수정
+    <div className='mypage'>
+        <MypageSidebar activeLink="회원정보 수정" />
+        <div className='infoMain'>
+            <div>
+                회원정보수정
+                아이디
+                닉네임
+                비밀번호
+                비밀번호확인
+                이메일
+                전화번호
+                주소
+                상세주소
+                탈퇴하기
+            </div>
+            <div>
+                프로필이미지
+                프로필이미지수정
+                저장하기
+            </div>
+        </div>
     </div>
 )
 };
--- src/components/mypage/InfoPwdCom.js
+++ src/components/mypage/InfoPwdCom.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import '../../css/login.css';
+import MypageSidebar from "../common/MypageSidebar"
+import { useNavigate } from 'react-router-dom';
+import '../../css/mypage.css';
+
 
 function InfoPwdCom() {
+    const navigate = useNavigate();
+
+    const handleInfoPwd = () => {
+        console.log("비밀번호 일치한지 확인");
+        navigate('/info');
+    };
     return (
-    <div>
-        마이페이지-회원정보 수정 전 패스워드 확인
+    <div className='mypagePwd'>
+        <MypageSidebar activeLink="회원정보 수정" />
+        <div className='infoPwdMain'>
+            <p>비밀번호를 입력해주세요.</p>
+            <input type="password" className='infoPwd' required placeholder="PW"/>
+            <button className='infoPwdBtn' onClick={handleInfoPwd}>확인</button>
+        </div>
     </div>
 )
 };
