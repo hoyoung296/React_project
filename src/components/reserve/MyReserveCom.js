d1151b18bb25482fe7bc906901e2aa52ff3991ae
sodam <wdam0910@naver.com>
2025-03-11 20:56:54 +0900

    내 예매내역 css 수정중

--- src/components/reserve/MyReserveCom.js
+++ src/components/reserve/MyReserveCom.js
@@ -18,28 +18,31 @@ const MyReserveCom = ({ list, start, reviewStatus, handlePageChange, del, showMo
                         const showCancelButton = now < new Date(startDateTime.getTime() - 30 * 60 * 1000)
                         const hasReview = data.reservationId in reviewStatus && reviewStatus[data.reservationId] === 0
                         return (
-                            <div className="ReserveDiv" key={data.reservationId}>
-                                <div className="ReserveInfoPart1Div">
-                                    <img src={`${data.posterUrl}`} alt="영화 포스터 이미지" />
-                                    <b>{data.title}</b>
+                            <div key={data.reservationId}>
+                                <div>
+                                    <img src={`${data.posterUrl}`} alt={`${data.title}`}/>
+                                    <p>{data.title}</p>
                                 </div>
-                                <div className="ReserveInfoPart2Div">
-                                    <p><b>예매번호 : {data.reservationId}</b>&nbsp;&nbsp;&nbsp;&nbsp;</p><br />
-                                    <p><b>상영관 : {data.screenName}</b></p><br />
-                                    <p><b>관람일 : {data.startDateTime}</b>&nbsp;&nbsp;&nbsp;&nbsp;<b>좌석 : {data.seatIds.join(", ")}</b></p><br />
-                                    <p><b>예매상태 :  {data.reservationStatusId === 1 ? "예매 진행 중" : "예매 완료"}</b></p>
+                                <div>
+                                    <p>예매번호 : {data.reservationId}</p>
+                                    <p>상영관 : {data.screenName}</p>
+                                    <p>관람일 : {data.startDateTime}</p>
+                                    <p>좌석 : {data.seatIds.join(", ")}</p>
+                                    <p>{data.reservationStatusId === 1 ? "결제 대기 중입니다." : ""}</p>
                                 </div>
                                 {list != null && (
                                     <div className="ReserveInfoPart3Div">
-                                        {showReviewButton && hasReview && data.paymentId !== undefined && <button onClick={() => showModal(data.title, data.posterUrl, data.director, data.actors, data.movieId)}>리뷰 쓰기</button>}
-                                        {showCancelButton && data.paymentId !== undefined && <button onClick={() => del(data.reservationId)}>예매 취소</button>}
-                                        {data.paymentId === undefined && <button onClick={() => onPayment(data)}>결제 진행</button>}
+                                        {showReviewButton && hasReview && data.paymentId !== undefined && 
+                                            <button onClick={() => showModal(data.title, data.posterUrl, data.director, data.actors, data.movieId)}>리뷰 쓰기</button>}
+                                        {showCancelButton && data.paymentId !== undefined && 
+                                            <button onClick={() => del(data.reservationId)}>예매 취소</button>}
+                                        {data.paymentId === undefined && 
+                                            <button onClick={() => onPayment(data)}>결제 진행</button>}
+                                            </div>
+                                        )}
                                     </div>
-                                )}
-                            </div>
-                        )
-                    })
-                }
+                                ); 
+                            })}
                 <Pagination start={start} list={list} handlePageChange={handlePageChange} />
             </div>
 
