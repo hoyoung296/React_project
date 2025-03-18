7fdf7200aa17b87e8664f1a15e9ab65151fa137d
hoyoung <skghdud024@naver.com>
2025-03-18 22:16:22 +0900

    Fix : 상세보기유저아이디에서 유저네임으로 수정

--- src/components/mainPage/HomePageCom.js
+++ src/components/mainPage/HomePageCom.js
@@ -2,12 +2,12 @@ import React from 'react'
 import EmblaCarousel from "../../components/mainPage/EmblaCarousel"
 import MovieList from "../../components/mainPage/MovieList"
 
-const HomePageCom = ({ TopMovies, RestMovies, Infolist, infoId, showInfo, hideInfo, onClick, relatedList, isModalOpen, modalType}) => {
+const HomePageCom = ({ TopMovies, list, Infolist, infoId, showInfo, hideInfo, onClick, relatedList, isModalOpen, modalType}) => {
     return (
         <>
             <EmblaCarousel list={TopMovies} Infolist={Infolist} infoId={infoId} showInfo={showInfo} hideInfo={hideInfo} 
             onClick={onClick} relatedList={relatedList} isModalOpen={isModalOpen} modalType={modalType} />
-            <MovieList list={RestMovies} Infolist={Infolist} infoId={infoId} showInfo={showInfo} hideInfo={hideInfo} 
+            <MovieList list={list} Infolist={Infolist} infoId={infoId} showInfo={showInfo} hideInfo={hideInfo} 
             onClick={onClick} relatedList={relatedList} isModalOpen={isModalOpen} modalType={modalType} />
         </>
     )
