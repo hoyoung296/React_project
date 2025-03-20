9684cd392f8318465bace01edeb44748b633c073
sodam <wdam0910@naver.com>
2025-03-20 19:36:31 +0900

    관리자 상영관리에서 상영일정 없는 영화 보는 필터 추가 / 영화관리에서 탑5 버튼 추가한 카피파일 추가(기존파일 수정X)

---
+++ src/components/Admin/AdminMovieCom___copy.js
@@ -0,0 +1,125 @@
+import { useState } from "react";
+import AdminSidebar from "./AdminSidebar"
+import "../../css/admin/admin.css"
+
+const AdminMovieCom = ({ list, editMovie, InputChange, EditClick, Update, manualUpdate, show, hide, handleChange, manualinsert, newMovie, delMovie }) => {
+
+    const [showTop5, setShowTop5] = useState(false);
+
+    const today = new Date();
+
+    const getTopMovies = (movies) => {
+        return movies
+        .map(movie => {
+            const [date, rank] = movie.movieRank.split("-");
+            const movieDate = new Date(date.slice(0,4), date.slice(4,6)-1, date.slice(6,8));
+            return {
+                ...movie,
+                    movieDate,
+            movieRank: parseInt(rank),
+            };
+        })
+        .filter(movie => movie.movieRank <= 5)
+        .sort((a, b) => Math.abs(today - a.movieDate) - Math.abs(today - b.movieDate))
+        .slice(0, 5);
+    };
+
+
+    return (
+        <div className="admindiv">
+            <AdminSidebar activeLink="영화관리" />
+            <div className="admindiv-1">
+                <h1>영화관리</h1>
+                {console.log(list)}
+                <button onClick={show} className="movieBtn">추가</button>
+                <button className="movieBtn" onClick={() => manualUpdate()}>업데이트</button>
+                <button className="movieBtn" onClick={() => setShowTop5(!showTop5)}>
+                    {showTop5 ? "전체 보기" : "탑 5 보기"}
+                </button>
+
+                <div className="table-wrapper">
+                    <table className="movie-table">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>제목</th>
+                                <th>영문제목</th>
+                                <th>포스터 URL</th>
+                                <th>스틸컷 URL</th>
+                                <th>시놉시스</th>
+                                <th>감독</th>
+                                <th>배우</th>
+                                <th>관리</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {(showTop5 ? getTopMovies(list) : list).length > 0 ? (
+                            (showTop5 ? getTopMovies(list) : list).map((data) => (
+                                <tr key={data.movieId}>
+                            {[
+                                { name: "movieId", value: data.movieId },
+                                { name: "title", value: data.title },
+                                { name: "entitle", value: data.entitle },
+                                { name: "posterUrl", value: data.posterUrl },
+                                { name: "stillUrl", value: data.stillUrl },
+                                { name: "movieSynopsis", value: data.movieSynopsis },
+                                { name: "directorName", value: data.directorName },
+                                { name: "actors", value: data.actors },
+                            ].map((item, index) => (
+                            <td key={index}>
+                                {editMovie?.movieId === data.movieId ? (
+                                    item.name === "movieSynopsis" ? (
+                                        <textarea name={item.name} value={item.value} onChange={(e) => InputChange(e, data.movieId)} />
+                                    ) : (
+                                        <input type="text" name={item.name} value={item.value} onChange={(e) => InputChange(e, data.movieId)} readOnly={item.name === "movieId"} />
+                                    )
+                                ) : (
+                                    <span className="ellipsis">{item.value}</span>
+                                )}
+                            </td>
+                            ))}
+                            <td>
+                                {editMovie?.movieId === data.movieId ? (
+                                    <button className="movieBtn" onClick={() => Update(data.movieId)}>수정 완료</button>
+                                ) : (
+                                    <button className="movieBtn" onClick={() => EditClick(data.movieId)}>수정</button>
+                                )}
+                                    <button className="movieBtn" onClick={() => delMovie(data.movieId)}>삭제</button>
+                            </td>
+                            </tr>
+                            ))
+                        ) : (
+                            <tr>
+                                <td colSpan="9">데이터가 없습니다.</td>
+                            </tr>
+                        )}
+
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+            <div className="modal">
+                <div className="newMovie">
+                    <h1>영화 추가</h1>
+                    <form onSubmit={manualinsert}>
+                        <input type="text" name="movieId" placeholder="영화 ID" value={newMovie.movieId} onChange={handleChange} required />
+                        <input type="text" name="title" placeholder="영화 제목" value={newMovie.title} onChange={handleChange} required />
+                        <input type="text" name="entitle" placeholder="영어 제목" value={newMovie.entitle} onChange={handleChange} required />
+                        <input type="text" name="posterUrl" placeholder="포스터 URL" value={newMovie.posterUrl} onChange={handleChange} required />
+                        <input type="text" name="stillUrl" placeholder="스틸컷 URL" value={newMovie.stillUrl} onChange={handleChange} required />
+                        <textarea name="movieSynopsis" placeholder="시놉시스" value={newMovie.movieSynopsis} onChange={handleChange} required />
+                        <input type="text" name="directorName" placeholder="감독" value={newMovie.directorName} onChange={handleChange} required />
+                        <input type="text" name="actors" placeholder="배우" value={newMovie.actors} onChange={handleChange} required />
+                        <input type="text" name="movieRank" placeholder="순위 (YYYYMMDD-R)" value={newMovie.movieRank} onChange={handleChange} required />
+                        <input type="text" name="openDt" placeholder="개봉일 (YYYY-MM-DD)" value={newMovie.openDt} onChange={handleChange} required /><br /><br />
+                        <button>등록</button>
+                        <button onClick={hide}>취소</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AdminMovieCom
